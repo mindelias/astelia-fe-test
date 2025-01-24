e9b9295b2343f19a596f6f1759b8af4cc1c39574
mindelias <mindelias@gmail.com>
2025-01-24 08:34:12 +0100

    Further cleanup

--- src/utils/data/graphnodes.tsx
+++ src/utils/data/graphnodes.tsx
@@ -1,4 +1,3 @@
-import { type Edge } from '@xyflow/react';
 import {
   ShieldAlert,
   Users,
@@ -6,12 +5,6 @@ import {
   VenetianMask as Vernetian,
 } from "lucide-react";
  
-export const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2' },
-  { id: 'e2-3', source: '2', target: '3' },
-] as Edge[];
-
-
 // Nodes
 const defaultNodes = [
     {
